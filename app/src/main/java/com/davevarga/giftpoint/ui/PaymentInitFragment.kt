74cca5c5bbac4ef65254ba36d6caa7abcda98554
davevarga666 <davevarga999@gmail.com>
2021-05-04 21:45:50 +0200

    Minor tweaks to API key handling.

--- app/src/main/java/com/davevarga/giftpoint/ui/PaymentInitFragment.kt
+++ app/src/main/java/com/davevarga/giftpoint/ui/PaymentInitFragment.kt
@@ -5,7 +5,6 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.Toast
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProviders
